ce44956760d57dedc9ebe0ac92bbe4ac3b607d6a
Andrew Ohakam <andrewohakam@outlook.com>
2021-11-30 18:41:06 -0500

    Changed navbar to use RR. add links to other pages

--- ldv-app/src/App.js
+++ ldv-app/src/App.js
@@ -1,32 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
+export default function App(){
+  return(
+    <Router>
       <nav id="navbar">
-        <a href="#">LifeDataVault</a> | 
-        <a href="#">Data</a> | 
-        <a href="#">Plans</a> | 
-        <a href="#">Explore</a> | 
-        <a href="#">Contact</a>
+          <a>
+            <Link to="/ldv">LifeDataVault</Link>
+          </a>
+          <a>
+            <Link to="/data">Data</Link>
+          </a>
+          <a>
+            <Link to="/plans">Plans</Link>
+          </a>
+          <a>
+            <Link to="/explore">Explore</Link>
+          </a>
       </nav>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+
+      <Routes>
+        <Route exact path="/ldv" element={<Home/>} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+function Home(){
+  return(
+  <div>
+   <h2 class="centerme blue-text">Life Data Vault</h2>
+   <p class="centerme grey-text">Leave your data for the ones you love the most</p>
+   <Link to="/plans">
+     <button>Get Started</button>
+   </Link>
+   
+   <button>Learn More</button>
+ </div>); 
+}
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <nav id="navbar">
+//         <a href="#">LifeDataVault</a> 
+//         <a href="#">Data</a> 
+//         <a href="#">Plans</a> 
+//         <a href="#">Explore</a> 
+//         <a href="#">Contact</a>
+//       </nav>
+
+//       <h2 class="centerme blue-text">Life Data Vault</h2>
+//       <p class="centerme grey-text">Leave your data for the ones you love the most</p>
+//       <button>Get Started</button>
+//       <button>Learn More</button>
+//     </div>
+//   );
+// }
