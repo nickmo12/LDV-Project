61e4903fd7d8a0f5ccb9bde44ec19c0e2661e6c1
Andrew Ohakam <andrewohakam@outlook.com>
2021-11-29 20:57:22 -0500

    Added navbar (this time with code)

--- ldv-app/src/App.js
+++ ldv-app/src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <nav id="navbar">
+        <a href="#">LifeDataVault</a> | 
+        <a href="#">Data</a> | 
+        <a href="#">Plans</a> | 
+        <a href="#">Explore</a> | 
+        <a href="#">Contact</a>
+      </nav>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +24,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
