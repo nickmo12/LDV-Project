8b3e814f453f50125ec0d46f9e4f50111d191bd6
Joel Rosas <joeli.rosas20@gmail.com>
2021-12-02 18:07:27 -0500

    Centered the image

--- ldv-app/src/App.js
+++ ldv-app/src/App.js
@@ -92,7 +92,7 @@ function Plans(){
 function Explore(){
   return(
     <div>
-      <img src={Photo} alt='Life Vault Cover Photo' class='Explore Image'/>
+      <img src={Photo} alt='Life Vault Cover Photo' class='ExploreImage'/>
       <p>
       Death is as somber as it is inevitable. But as we live more of our lives online, it's more important than ever to make sure loved ones can access digital accounts when we're gone. Don't be the guy who locked cryptocurrency exchange customers out of $250 million after his death because only he knew the password.
       </p>
